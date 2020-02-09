d801afa3a9fc883fe5d77503042eef2b3e055300
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-02-09 13:45:02 +0100

    Finalized styling && added 404 checker

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Axios from "axios";
 import Form from "./components/Form";
 import Movies from "./components/Movies";
 import "./App.css";
-
-const OMDB_API_KEY = "39aae644";
-
 /* 
   1. Base App Component for our application.
 
@@ -14,11 +11,13 @@ const OMDB_API_KEY = "39aae644";
   3. Update the state of the App component with user input.
       - Some logic in the event the user writes.
 
-  4. Retrieve data from API data using the OMDB_API_KEY.
+  4. Retrieve data from API data using the API_KEY.
       - Some logic in the event the user decides to search.
 
   5. Display the retrieved movie
 */
+
+const API_KEY = "39aae644";
 
 // 1. Base Component
 class App extends Component {
@@ -39,7 +38,7 @@ class App extends Component {
     event.preventDefault();
 
     Axios.get(
-      `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${this.state.userInput}`
+      `http://www.omdbapi.com/?apikey=${API_KEY}&t=${this.state.userInput}`
     )
       .then(response => this.setState({ retrievedMovieData: response.data }))
       .catch(error => console.error(`Something went wrong ${error}`));
@@ -49,7 +48,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App_header">
-          <h1>Find Your Favorite Movie</h1>
+          <h1>M<span role="img" aria-label="coder">🎥</span>vieFinder</h1>
 
           {/* 2. Form for submitting data*/}
           <Form
