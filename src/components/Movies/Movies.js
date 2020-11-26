417ad03607cc6844c08e5690898f45e1da4b92b6
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-11-26 20:45:08 +0100

    Turned Movies.js into Functional Component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,31 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import MovieNotFound from "./MovieNotFound/MovieNotFound";
 import MovieFound from "./MovieFound/MovieFound";
 import SearchInfo from "../../pages/SearchInfoPage/SearchInfo";
 import "./Movies.css";
 
-class Movies extends Component {
-  state = {
-    movieToShow: ""
-  };
+const Movies = (props) => {
+  const { movies } = props;
+  const [movieToShow, setMovieToShow] = useState("");
 
-  componentDidUpdate(prevProps) {
-    if (this.props.movies !== prevProps.movies) {
-      this.setState({ movieToShow: this.props.movies });
-    }
-  }
+  const displayMovieResults = () => {
+        return movieToShow.Error ? (
+          <MovieNotFound notFound={movieToShow.Error} />
+        ) : (
+          <MovieFound foundMovie={movieToShow} />
+        );
+      };
 
-  displayMovieResults = () => {
-    return this.state.movieToShow.Error ? (
-      <MovieNotFound notFound={this.state.movieToShow.Error} />
-    ) : (
-      <MovieFound foundMovie={this.state.movieToShow} />
-    );
-  };
-
-  render() {
-    return this.state.movieToShow ? this.displayMovieResults() : <SearchInfo />;
-  }
+  useEffect(() => {
+      setMovieToShow(movies)
+  }, [movies])
+  return (
+    movieToShow ? displayMovieResults() : <SearchInfo />
+  );
 }
 
 export default Movies;
